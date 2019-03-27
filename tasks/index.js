8d8b6e2935d2965c57a7534c8569c26611dff0a8
Steen Klingberg <steen@sk-ab.se>
2019-03-27 09:29:10 +0100

    child_asset_rel

--- tasks/index.js
+++ tasks/index.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
 
 			dom.sandbox('', {}, function (jsdom) {
 				const $$$ = require('tripledollar');
-				const {asset, asset_feature, storage_item, asset_element} = require('../lib/asset');
+				const {asset, asset_feature, storage_item, asset_element, child_asset_rel} = require('../lib/asset');
 				const {dialog} = require('../lib/dialog');
 				const {component} = require('../lib/component');
 				const {widgetitem} = require('../lib/widget');
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
 
 						// component
 						let c_uuid = uuid();
-						widget.component_file = widget.component_file || name + '._component.xml';
+						widget.component_file = widget.component_file || name + '_component.xml';
 
 						let c = asset({
 								name: widget.name,
@@ -136,30 +136,42 @@ module.exports = function (grunt) {
 
 						// dialog location
 						let locfile = path.resolve(__dirname, '../../../censhare/dialogs/' + (data.prefix||'common') + '_localization.xml');
-						if (!fs.existsSync(locfile)) {
-							let loc_uuid = uuid();
-							let loc = asset({
-								name: 'Online Channel Management Localization',
-								type: 'module.localization.',
-								application: 'texteditor'
-							})
-							.ins(asset_element())
-							.ins(asset_feature({
-								feature: 'censhare:uuid',
-								value_string: loc_uuid
-							}))
-							.ins(storage_item({
-								mimetype: 'application/xml',
-								relpath: 'file:' + loc_uuid.substring(0, 8) + '_' + (data.prefix||'common') + 'localization.xml'
-							}));
-							let loccontent = localization(widget);
-							saveFile('censhare/dialogs/' + (data.prefix||'common') + '_localization.xml', loccontent.outerHTML);
-							saveFile('censhare/assets/' + (data.prefix||'common') + '_dialog_localization_asset.xml', loc.outerHTML);
-						}
+						let locassetfile = path.resolve(__dirname, '../../../censhare/assets/' + (data.prefix||'common') + '_localization_asset.xml')
+						var locfilerel;
+						if (!fs.existsSync(locfile) || !fs.existsSync(locassetfile)) {
+							if (!fs.existsSync(locassetfile)) {
+								let loc_uuid = uuid();
+								locfilerel = 'file:' + loc_uuid + '.asset.xml';
+								let loc = asset({
+									name: 'Online Channel Management Localization',
+									type: 'module.localization.',
+									application: 'texteditor'
+								})
+								.ins(asset_element())
+								.ins(asset_feature({
+									feature: 'censhare:uuid',
+									value_string: loc_uuid
+								}))
+								.ins(storage_item({
+									mimetype: 'application/xml',
+									relpath: 'file:' + loc_uuid.substring(0, 8) + '_' + (data.prefix||'common') + '_localization.xml'
+								}));
+								saveFile('censhare/assets/' + (data.prefix||'common') + '_localization_asset.xml', loc.outerHTML);
+							}
+							if (!fs.existsSync(locfile)) {
+								let loccontent = localization(widget);
+								saveFile('censhare/dialogs/' + (data.prefix||'common') + '_localization.xml', loccontent.outerHTML);
+							}
+						} else {
+							let curr_locfile = fs.readFileSync(locassetfile);
+							dom.sandbox(curr_locfile.toString(), {}, function (jd) {
+								locfilerel = 'file:' + jd.window.document.querySelector("asset_feature[feature='censhare:uuid']").getAttribute('value_string') + '.asset.xml';
+							});
+						};
 
 						// dialog
 						let d_uuid = uuid();
-						widget.widget_file = widget.widget_file || name + '_dialog.xml';
+						widget.dialog_file = widget.dialog_file || name + '_dialog.xml';
 
 						let d = asset({
 								name: widget.name,
@@ -190,6 +202,10 @@ module.exports = function (grunt) {
 								feature: 'censhare:output-channel',
 								value_key: data.channel
 							}))
+							.ins(child_asset_rel({
+								key: 'user.localization.',
+								child_asset_ref_file: locfilerel
+							}))		
 							.ins(storage_item({
 								mimetype: 'application/xml',
 								relpath: 'file:' + d_uuid.substring(0, 8) +'_' + widget.dialog_file
